c141d6fdfb6ac9e37f73874de7b76dc8006aa16f
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2022-02-09 11:38:01 +0100

    squashed single decouple commit

--- src/Spryker/Zed/DummyPayment/Communication/Plugin/Oms/Command/RefundPlugin.php
+++ src/Spryker/Zed/DummyPayment/Communication/Plugin/Oms/Command/RefundPlugin.php
@@ -24,15 +24,15 @@ class RefundPlugin extends AbstractPlugin implements CommandByOrderInterface
      *
      * @api
      *
-     * @param array<\Orm\Zed\Sales\Persistence\SpySalesOrderItem> $orderItems
+     * @param array<\Orm\Zed\Sales\Persistence\SpySalesOrderItem> $salesOrderItems
      * @param \Orm\Zed\Sales\Persistence\SpySalesOrder $orderEntity
      * @param \Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject $data
      *
      * @return array
      */
-    public function run(array $orderItems, SpySalesOrder $orderEntity, ReadOnlyArrayObject $data)
+    public function run(array $salesOrderItems, SpySalesOrder $orderEntity, ReadOnlyArrayObject $data)
     {
-        $this->getFacade()->refund($orderItems, $orderEntity);
+        $this->getFacade()->refund($salesOrderItems, $orderEntity);
 
         return [];
     }
